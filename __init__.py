8baadfbca7be589d40f1ec62469a027748a6172b
shravanikadam1907-student <shravanikadam1907@gmail.com>
2026-04-21 22:36:05 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .celsius_to_fahrenheit import celsius_to_fahrenheit
+from.fahrenheit_to_celsius import fahrenheit_to_celsius
+from.celsius_to_kelvin import celsius_to_kelvin
+
+print(celsius_to_fahrenheit(25))
+print(fahrenheit_to_celsius(77))
+print(celsius_to_kelvin(25))
